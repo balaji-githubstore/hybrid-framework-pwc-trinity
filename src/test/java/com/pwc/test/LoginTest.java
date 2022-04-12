ce8b67fc6c3a6d178b2378ff23899ac876c7e8f6
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-04-12 14:54:10 +0530

    created test method and validated title- complete invalid test

---
+++ src/test/java/com/pwc/test/LoginTest.java
@@ -0,0 +1,31 @@
+package com.pwc.test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+	@Test
+	public void invalidCredentialTest() {
+		
+		WebDriverManager.chromedriver().setup();
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		
+//		2. Enter username John
+//		3. Enter password admin123
+//		4. Click on login
+//		5. Assert.assertEquals("", "Invalid credentials");
+
+	}
+
+}
---
+++ src/test/java/com/pwc/test/LoginUserInterfaceTest.java
@@ -0,0 +1,27 @@
+package com.pwc.test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginUserInterfaceTest {
+
+	@Test
+	public void validateTitleTest()
+	{
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		
+		String actualTitle= driver.getTitle();
+		Assert.assertEquals(actualTitle, "OrangeHRM");
+	}
+}
