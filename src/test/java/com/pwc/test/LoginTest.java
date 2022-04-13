3812541f081e3639634692288b67d902e6104973
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-04-13 07:16:18 +0530

    browserconfig done

--- src/test/java/com/pwc/test/LoginTest.java
+++ src/test/java/com/pwc/test/LoginTest.java
@@ -1,27 +1,15 @@
 package com.pwc.test;
 
-import java.time.Duration;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class LoginTest {
-
+import com.pwc.base.AutomationWrapper;
+	
+public class LoginTest extends AutomationWrapper {
+	
 	@Test
-	public void invalidCredentialTest() {
-		
-		WebDriverManager.chromedriver().setup();
-
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-		driver.get("https://opensource-demo.orangehrmlive.com/");
-		
+	public void invalidCredentialTest() {	
 		driver.findElement(By.id("txtUsername")).sendKeys("john");
 		driver.findElement(By.id("txtPassword")).sendKeys("john123");
 		driver.findElement(By.id("btnLogin")).click();
--- src/test/java/com/pwc/test/LoginUserInterfaceTest.java
+++ src/test/java/com/pwc/test/LoginUserInterfaceTest.java
@@ -1,26 +1,15 @@
 package com.pwc.test;
 
-import java.time.Duration;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class LoginUserInterfaceTest {
+import com.pwc.base.AutomationWrapper;
+	
+public class LoginUserInterfaceTest extends AutomationWrapper {
 
 	@Test
 	public void validateTitleTest()
-	{
-		WebDriverManager.chromedriver().setup();
-		
-		WebDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-		driver.get("https://opensource-demo.orangehrmlive.com/");
-		
+	{	
 		String actualTitle= driver.getTitle();
 		Assert.assertEquals(actualTitle, "OrangeHRM");
 	}
