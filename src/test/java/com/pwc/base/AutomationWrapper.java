f0215fb0c3352822587cca5a983e8662db2eb343
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-04-13 12:44:37 +0530

    cross browser config

--- src/test/java/com/pwc/base/AutomationWrapper.java
+++ src/test/java/com/pwc/base/AutomationWrapper.java
@@ -4,6 +4,8 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -13,19 +15,27 @@ public class AutomationWrapper {
 	protected WebDriver driver;
 
 	@BeforeMethod
-	public void setup()
-	{
-		WebDriverManager.chromedriver().setup();
+	public void setup() {
+		String browserName = "edge";
 
-		driver = new ChromeDriver();
+		if (browserName.equalsIgnoreCase("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		} else if (browserName.equals("ff")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 	}
-	
+
 	@AfterMethod
-	public void teardown()
-	{
+	public void teardown() {
 		driver.quit();
 	}
 }
