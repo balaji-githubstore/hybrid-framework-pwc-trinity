f2edba95ea525587276ac6ff1813b6ecc28ab214
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-04-13 08:16:39 +0530

    dataprovider in logintest class

--- src/test/java/com/pwc/test/LoginTest.java
+++ src/test/java/com/pwc/test/LoginTest.java
@@ -2,33 +2,46 @@ package com.pwc.test;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.pwc.base.AutomationWrapper;
-	
+
 public class LoginTest extends AutomationWrapper {
-	
-	@Test
-	public void invalidCredentialTest() {	
-		driver.findElement(By.id("txtUsername")).sendKeys("john");
-		driver.findElement(By.id("txtPassword")).sendKeys("john123");
-		driver.findElement(By.id("btnLogin")).click();
+
+	@DataProvider
+	public String[][] invalidCredentialData()
+	{
+		String[][] main=new String[2][3]; 
 		
-		String actualError= driver.findElement(By.id("spanMessage")).getText();
-		Assert.assertEquals(actualError, "Invalid credentials");
+		main[0][0]="john";
+		main[0][1]="john123";
+		main[0][2]="Invalid credentials";
+		
+		main[1][0]="Peter";
+		main[1][1]="Peter123";
+		main[1][2]="Invalid credentials";
+		
+		return main;
 	}
 	
+	@Test(dataProvider = "invalidCredentialData")
+	public void invalidCredentialTest(String username, String password, String expectedError) {
+		driver.findElement(By.id("txtUsername")).sendKeys(username);
+		driver.findElement(By.id("txtPassword")).sendKeys(password);
+		driver.findElement(By.id("btnLogin")).click();
+
+		String actualError = driver.findElement(By.id("spanMessage")).getText();
+		Assert.assertEquals(actualError, expectedError);
+	}
+
 	@Test
-	public void validCredentialTest()
-	{
+	public void validCredentialTest() {
 		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
 		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
 		driver.findElement(By.id("btnLogin")).click();
-		
+
 		Assert.assertEquals(driver.getCurrentUrl(), "https://opensource-demo.orangehrmlive.com/index.php/dashboard");
-		
 	}
 
 }
-	
-
