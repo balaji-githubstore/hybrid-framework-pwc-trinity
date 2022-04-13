a1ce1d1ae597a30c8f10e8cbc47f1ead5f964d6d
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-04-13 10:30:54 +0530

    getSheetIntoTwoDimArray

--- src/test/java/com/pwc/utilities/ExcelUtils.java
+++ src/test/java/com/pwc/utilities/ExcelUtils.java
@@ -8,28 +8,25 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelUtils {
-	
-	public static String[][] getSheetIntoTwoDimArray(String path,String sheetname) throws IOException
-	{
-		FileInputStream file=new FileInputStream(path);
-		XSSFWorkbook book=new XSSFWorkbook(file);
-		XSSFSheet sheet= book.getSheet(sheetname);
-		int rowCount=sheet.getPhysicalNumberOfRows();
-		int cellCount=sheet.getRow(0).getPhysicalNumberOfCells();
-		
-		DataFormatter format=new DataFormatter();
-		
-		String[][] main=new String[rowCount-1][cellCount];
-		
-		for(int r=1;r<rowCount;r++)
-		{
-			for(int c=0;c<cellCount;c++)
-			{	
-				String cellValue=format.formatCellValue(sheet.getRow(r).getCell(c));
-				main[r-1][c]=cellValue;
+
+	public static String[][] getSheetIntoTwoDimArray(String path, String sheetname) throws IOException {
+		FileInputStream file = new FileInputStream(path);
+		XSSFWorkbook book = new XSSFWorkbook(file);
+		XSSFSheet sheet = book.getSheet(sheetname);
+		int rowCount = sheet.getPhysicalNumberOfRows();
+		int cellCount = sheet.getRow(0).getPhysicalNumberOfCells();
+
+		DataFormatter format = new DataFormatter();
+
+		String[][] main = new String[rowCount - 1][cellCount];
+
+		for (int r = 1; r < rowCount; r++) {
+			for (int c = 0; c < cellCount; c++) {
+				String cellValue = format.formatCellValue(sheet.getRow(r).getCell(c));
+				main[r - 1][c] = cellValue;
 			}
 		}
-		
+
 		return main;
 	}
 
